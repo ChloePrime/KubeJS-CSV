c42a1bf5f2b3597f21cf7fed32463201dae2cf61
Chloe Prime <chloe_von_einzbern@qq.com>
2025-09-12 13:23:22 +0800

    修复 .d.ts 文档的方法缺少 static 的问题
    (cherry picked from commit 1c3d867ccf9608fbbf67620e0b5fbf01d363d0c1)

--- src/main/resources/docs/api.d.ts
+++ src/main/resources/docs/api.d.ts
@@ -4,7 +4,7 @@ export class CsvIO {
      * 
      * @param document the CSV document.
      */
-    parse(document: string): Array<any>
+    public static parse(document: string): Array<any>
 
     /**
      * Read a CSV document from the file system. See docs/usage.md in the mod jar for an example.
@@ -12,7 +12,7 @@ export class CsvIO {
      * 
      * @param path path to the CSV file.
      */
-    read(path: string|java.nio.file.Path): Array<any>
+    public static read(path: string|java.nio.file.Path): Array<any>
 
     /**
      * Serialize an array of JavaScript objects to a CSV document string.
@@ -20,7 +20,7 @@ export class CsvIO {
      * @param content the array of JavaScript objects.
      * @returns the CSV document, with all keys found in every objects in {@link content}
      */
-    toString(content: Array<any>): string
+    public static toString(content: Array<any>): string
 
     /**
      * Write an array of JavaScript objects as a CSV document to the file system.
@@ -29,7 +29,7 @@ export class CsvIO {
      * @param path path to the CSV file.
      * @param content the array of JavaScript objects.
      */
-    write(path: string|java.nio.file.Path, content: Array<any>): void
+    public static write(path: string|java.nio.file.Path, content: Array<any>): void
 
     /**
      * Deletes a CSV document from the file system.
@@ -37,5 +37,5 @@ export class CsvIO {
      * 
      * @param path path to the CSV file.
      */
-    delete(path: string|java.nio.file.Path): void
+    public static delete(path: string|java.nio.file.Path): void
 }
