5bdc23ea0684f56cc2639f497739af93aa83537f
Chloe Prime <chloe_von_einzbern@qq.com>
2026-06-28 08:35:58 +0800

    Add example script to git
    (cherry picked from commit 01897dc22e437abdb5b1f6a8049f65f632ffa126)

---
+++ run/kubejs/server_scripts/csv_example.js
@@ -0,0 +1,10 @@
+// Visit the wiki for more info - https://kubejs.com/
+console.info('Hello, testing KubeJS CSV functionality!')
+
+console.info(CsvIO.read("kubejs/test.csv"))
+CsvIO.write("kubejs/test_write.csv", [
+  {Name: "Alice", Phone: 10000, Bool: true},
+  {Name: "Bob", Phone: 10001, Bool: false},
+  {Name: "Charlie", Phone: 10086},
+  {Name: "Bad Ass", Phone: "", Bool: undefined, Blue: {A: 1, C: false}},
+])
